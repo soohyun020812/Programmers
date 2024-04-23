1d0fa6efb344a273e1cb7877d1965d3f29421684
SooHyun Lee <131852352+soohyun020812@users.noreply.github.com>
2024-04-23 12:21:37 +0900

    Add files via upload

---
+++ Python/문자열 내 p와 y의 개수.py
@@ -0,0 +1,42 @@
+"""
+문제 설명
+대문자와 소문자가 섞여있는 문자열 s가 주어집니다. 
+s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 
+'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 
+단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+제한사항
+문자열 s의 길이 : 50 이하의 자연수
+문자열 s는 알파벳으로만 이루어져 있습니다.
+
+def solution(s):
+    answer = True
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return True
+"""
+
+
+def solution(s):
+    s = s.lower()  # 모두 소문자로 변환
+    count_p = s.count("p")  # 'p'의 개수 세기
+    count_y = s.count("y")  # 'y'의 개수 세기
+
+    return count_p == count_y
+
+
+# 다른사람 코드 풀이
+# 개편된 문제에 대한 풀이
+"""
+def numPY(s):
+    # 함수를 완성하세요
+    return s.lower().count('p') == s.lower().count('y')
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( numPY("pPoooyY") )
+print( numPY("Pyy") )
+"""
